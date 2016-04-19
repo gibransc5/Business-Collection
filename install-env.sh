bb45e5a909a92c41d3d27b5648536f43b8b59ec6
Gibran Rodriguez <brangi000@gmail.com>
2016-04-20 01:15:18 +0300

    Changes: installation options for the 2 tested OS          Ubuntu 14 and Mac el capitan

--- install-env.sh
+++ install-env.sh
@@ -1,7 +1,29 @@
 #!/bin/bash                                                                     
 # Install software dependecies and app compilation
 
-sh install/install-otp17.sh
-sh install/install-sofware-deps.sh 
+#sh install/install-otp17.sh
+#sh install/install-sofware-deps.sh
+
+#if [ "$*" == "" ]; then
+#        echo "No arguments provided"
+#            exit 1
+#fi
+
+if [ "$1" == "ubuntu" ]; 
+then  echo "Installing on Ubuntu"
+    apt-get install curl
+    apt-get install wget
+    sh install/install-otp17.sh
+    sh install/install-sofware-deps.sh
+fi
+if [ "$1" == "mac" ];
+then echo "Installing on Mac"
+    brew update
+    brew install homebrew/versions/erlang-r17 
+    brew install riak
+    ulimit -n 65536
+    riak start
+fi
+
 rebar get-deps
 rebar compile
