634c0ff2c01f45b705f4310dd9e46493a85ac79d
Gibran Rodriguez <brangi000@gmail.com>
2016-04-20 10:39:19 +0300

    Fix installation script on ubuntu: setting up leveldb option for indexing support

--- install/install-sofware-deps.sh
+++ install/install-sofware-deps.sh
@@ -6,6 +6,8 @@ apt-get install rebar -y
 apt-get install git -y
 apt-get install riak -y
 
+sed -i 's/storage_backend = bitcask/storage_backend = leveldb/g' /etc/riak/riak.conf
+
 ulimit -n 65536
 
 riak start
