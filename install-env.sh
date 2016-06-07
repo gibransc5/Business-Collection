5108295ac9ef48c03d78c7a4a7545a4197b57830
gibransc5 <gibran.rodriguez@sc5.io>
2016-06-07 12:24:41 +0300

    Update install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -12,9 +12,13 @@ if [ "$1" == "mac" ];
 then echo "Installing on Mac"
     brew update
     brew install homebrew/versions/erlang-r17 
+    echo kern.maxfiles=65536 | sudo tee -a /etc/sysctl.conf
+    echo kern.maxfilesperproc=65536 | sudo tee -a /etc/sysctl.conf
+    sudo sysctl -w kern.maxfiles=65536
+    sudo sysctl -w kern.maxfilesperproc=65536
+    ulimit -n 65536 65536   
     brew install riak
     sed -i '' 's/storage_backend = bitcask/storage_backend = leveldb/g' /usr/local/Cellar/riak/2.1.3/libexec/etc/riak.conf 
-    ulimit -n 65536
     riak start
 fi
 
